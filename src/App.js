fea16a3709aed04830df1175f7b1daeeb9b8f0c7
palindrome-kasak <64642473+palindrome-kasak@users.noreply.github.com>
2022-01-23 00:55:09 +0530

    cc

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from '../src/pages/Home'
 import Shop from '../src/pages/Shop'
 import Footer from '../src/Components/fixedComponents/Footer';
-
+import Product from '../src/Components/ShopComponents/Product';
 
 
 
@@ -20,6 +20,12 @@ function App() {
           {/* <Route path="/connect" component={Cafe} /> */}
           <Route path="/shop" component={Shop} />
         </Routes>
+        <div>{<Product/>}</div> 
+             <div>{<Product/>}</div> 
+             <div>{<Product/>}</div> 
+             <div>{<Product/>}</div> 
+             <div>{<Product/>}</div> 
+             <div>{<Product/>}</div> 
       <Footer/>
       
     </div>
