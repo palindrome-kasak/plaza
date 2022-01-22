7ae51ef9e033685ae7ea80974a31572ba86b54be
palindrome-kasak <64642473+palindrome-kasak@users.noreply.github.com>
2022-01-23 00:40:17 +0530

    routing added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import './App.css';
-import NavBar from '../src/Components/NavBar'
+// import NavBar from '../src/Components/NavBar'
+import NavBar from '../src/Components/fixedComponents/NavigationBar';
+import { Route, Routes } from "react-router-dom";
+import Home from '../src/pages/Home'
+import Shop from '../src/pages/Shop'
+import Footer from '../src/Components/fixedComponents/Footer';
+
+
+
+
 
 function App() {
   return (
     <div>
      <NavBar/>
+        <Routes>
+          <Route exact path="/" component={Home} />
+          <Route path="/home" component={Home} />
+          {/* <Route path="/connect" component={Cafe} /> */}
+          <Route path="/shop" component={Shop} />
+        </Routes>
+      <Footer/>
+      
     </div>
   );
 }
