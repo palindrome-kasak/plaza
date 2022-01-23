9843c2440a6aeae9963ab41d1c32def979dbc769
Warisha32 <66689362+Warisha32@users.noreply.github.com>
2022-01-23 12:52:22 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,33 +1,29 @@
-import './App.css';
+import "./App.css";
 // import NavBar from '../src/Components/NavBar'
-import NavBar from '../src/Components/fixedComponents/NavigationBar';
+import NavBar from "../src/Components/fixedComponents/NavigationBar";
 import { Route, Routes } from "react-router-dom";
-import Home from '../src/pages/Home'
-import Shop from '../src/pages/Shop'
-import Footer from '../src/Components/fixedComponents/Footer';
-import Product from '../src/Components/ShopComponents/Product';
-
-
-
+import Home from "../src/pages/Home";
+import Shop from "../src/pages/Shop";
+import Footer from "../src/Components/fixedComponents/Footer";
+import Product from "../src/Components/ShopComponents/Product";
 
 function App() {
   return (
     <div>
-     <NavBar/>
-        <Routes>
-          <Route exact path="/" component={Home} />
-          <Route path="/home" component={Home} />
-          {/* <Route path="/connect" component={Cafe} /> */}
-          <Route path="/shop" component={Shop} />
-        </Routes>
-        <div>{<Product/>}</div> 
-             <div>{<Product/>}</div> 
-             <div>{<Product/>}</div> 
-             <div>{<Product/>}</div> 
-             <div>{<Product/>}</div> 
-             <div>{<Product/>}</div> 
-      <Footer/>
-      
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+
+        <Route path="/home" element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
+      </Routes>
+      <div>{<Product />}</div>
+      <div>{<Product />}</div>
+      <div>{<Product />}</div>
+      <div>{<Product />}</div>
+      <div>{<Product />}</div>
+      <div>{<Product />}</div>
+      <Footer />
     </div>
   );
 }
