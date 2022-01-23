1c97b1c4c2887533b6a1d1f7724735cf6ca5fd08
palindrome-kasak <64642473+palindrome-kasak@users.noreply.github.com>
2022-01-24 03:45:59 +0530

    cc

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import Footer from '../Components/fixedComponents/Footer';
 import Product from '../Components/ShopComponents/Product';
 
-
+import Banner from '../Components/fixedComponents/Banner'
 
 
 const Home = () => {
   return <div>
+  <Banner/>
        <div>{<Product />}</div>
       <div>{<Product />}</div>
       <div>{<Product />}</div>
