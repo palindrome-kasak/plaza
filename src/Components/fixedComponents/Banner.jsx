1bb04505567c7266324c8d1c3b4acaaacf286ba5
palindrome-kasak <64642473+palindrome-kasak@users.noreply.github.com>
2022-01-30 20:10:35 +0530

    cc

--- src/Components/fixedComponents/Banner.jsx
+++ src/Components/fixedComponents/Banner.jsx
@@ -21,7 +21,7 @@ class ImageGallaryComponent extends React.Component {
         className="carousel-banner">
                     <div   id="banner-container">
                         <img src={LandingImage1} alt="LandingImage" />
-                        <div id="banner-text">My Classic Still 1</div>
+                        <div className="legend">My Classic Still 1</div>
                     </div>
                     <div   id="banner-container">
                         <img src={LandingImage2}  alt="LandingImage" />
