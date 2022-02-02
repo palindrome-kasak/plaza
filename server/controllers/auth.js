4f6e15b32fe5f15b8dd050cdc0f31f9301056995
ashuxldr <ashuxldr@gmail.com>
2022-02-02 19:11:05 +0530

    added category and products models

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -51,3 +51,20 @@ exports.logout = (req, res) => {
     message: "USER SIGNOUT SUCCESSFULLY",
   });
 };
+
+// PROTECTED ROUTES
+exports.isSignedIn = expressJwt({
+  secret: process.env.SECRET,
+  userProperty: "auth",
+});
+
+// CUSTOM MIDDLEWARES
+exports.isAuthenticated = (req, res, next) => {
+  let checker = req.profile && req.auth && req.profile._id == req.auth._id;
+  if (!checker) {
+    return res.status(403).json({
+      error: "ACCESS DENIED",
+    });
+  }
+  next();
+};
