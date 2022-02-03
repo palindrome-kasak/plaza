89fa8930583c1b6712b2ba00e17660f12c9a2190
palindrome-kasak <64642473+palindrome-kasak@users.noreply.github.com>
2022-02-03 16:12:30 +0530

    redux main files

--- src/index.js
+++ src/index.js
@@ -5,9 +5,15 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom";
 
+import {Provider} from 'react-redux';
+import store from './redux/store'
+
+
 ReactDOM.render(
   <BrowserRouter>
+  <Provider store={store}>
     <App />
+    </Provider>
     </BrowserRouter>,
   document.getElementById('root')
 );
