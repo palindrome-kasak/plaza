f66b237a27215c1b3aa9d41d4d1533726d7616a0
palindrome-kasak <64642473+palindrome-kasak@users.noreply.github.com>
2022-02-06 00:06:31 +0530

    shop

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,9 +1,51 @@
-import React from 'react';
+// import React from 'react';
+// import Product from '../Components/ShopComponents/Product';
 
-const Shop = () => {
-  return <div> 
-  hi shop
-  </div>;
+// // Redux
+// import { connect } from "react-redux";
+
+
+// const Shop = ({ products }) => {
+//   return (
+//     <div>
+//       {products.map((product) => (
+//         <Product key={product.id} product={product} />
+//       ))}
+//     </div>
+//   );
+// };
+
+// const mapStateToProps = (state) => {
+//   return {
+//     products: state.shop.products,
+//   };
+// };
+
+// export default connect(mapStateToProps)(Shop);
+
+
+import React from "react";
+// import styles from "./products.module.css";
+ import Product from '../Components/ShopComponents/Product';
+
+// Redux
+import { connect } from "react-redux";
+
+
+const Shop = ({ products }) => {
+  return (
+    <div >
+      {products.map((product) => (
+        <Product key={product.id} product={product} />
+      ))}
+    </div>
+  );
 };
 
-export default Shop;
+const mapStateToProps = (state) => {
+  return {
+    products: state.shop.products,
+  };
+};
+
+export default connect(mapStateToProps)(Shop);
