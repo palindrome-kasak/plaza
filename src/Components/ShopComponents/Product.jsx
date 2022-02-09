5f9f8212fbbd286cda75a3daca5fda6ab3d23b5f
Warisha32 <66689362+Warisha32@users.noreply.github.com>
2022-02-10 03:18:35 +0530

    Update Product.jsx

--- src/Components/ShopComponents/Product.jsx
+++ src/Components/ShopComponents/Product.jsx
@@ -2,24 +2,36 @@ import React from 'react';
 import "../../styles/main.scss";
 import WineGlass from '../../assets/Images/wineGlass.png'
 import { Link } from 'react-router-dom';
-
-const Product = () => {
+import { connect } from "react-redux";
+import {
+  loadCurrentItem,
+  addToCart,
+} from "../../Redux/Shopping/Actions";
+const Product = ({ product, addToCart, loadCurrentItem }) => {
     return(
 
 <div className="column">
 <div className="productImage">
-<img className="ProductPicture" src={WineGlass} alt="img" />
+<img className="ProductPicture" src={product.image}  alt="img" />
 </div>
 <div className="productText">
     <div className="Head">
-    <div className="chronicles_head">Plaza Chronicles</div> <div className="priceTag">$15</div><br></br>
+    <div className="chronicles_head">{product.title} </div> <div className="priceTag">{product.price} </div><br></br>
     </div>
-    <div>Wanna be cool? Get this.<br></br> Trust us.</div>
+    <div>{product.description}</div>
 </div>
 <div className="button">
-<Link to="/productDetails" className="Buy" ><div className="buyText">Buy</div></Link>
+{/* <Link to="/productDetails" className="Buy" ><div className="buyText">Buy</div></Link> */}
+<button className="Buy"onClick={() => addToCart(product.id)}><div className="buyText">Buy</div></button>
 </div>
 </div>
     )
  };
-  export default Product
+ const mapDispatchToProps = (dispatch) => {
+    return {
+      addToCart: (id) => dispatch(addToCart(id)),
+      loadCurrentItem: (item) => dispatch(loadCurrentItem(item)),
+    };
+  };
+
+  export default connect(null, mapDispatchToProps)(Product);
