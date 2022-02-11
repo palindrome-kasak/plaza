d97a74c85dec5b5bfe6a7feb99bf203fbe248e6a
palindrome-kasak <64642473+palindrome-kasak@users.noreply.github.com>
2022-02-11 18:26:38 +0530

    cc

--- src/Components/ShopComponents/Product.jsx
+++ src/Components/ShopComponents/Product.jsx
@@ -4,12 +4,12 @@ import WineGlass from '../../assets/Images/wineGlass.png'
 import { Link } from 'react-router-dom';
 
 // Redux
-import { connect } from "react-redux";
+// import { connect } from "react-redux";
 
-import {
-    loadCurrentItem,
-    addToCart,
-  } from '../../redux/Shopping/shopping-action';
+// import {
+//     loadCurrentItem,
+//     addToCart,
+//   } from '../../redux/Shopping/shopping-action';
 
 
 // const Product = ({ product }) => {
@@ -31,52 +31,73 @@ import {
 //     )
 // };
 
-import styles from './products.module.css';
+// import styles from './products.module.css';
 
 
-const Product = ({ product, addToCart, loadCurrentItem }) => {
-    return (
-      <div className={styles.product}>
-        <img
-          className={styles.product__image}
-          src={product.image}
-          alt={product.title}
-        />
+// const Product = ({ product, addToCart, loadCurrentItem }) => {
+//     return (
+//       <div className={styles.product}>
+//         <img
+//           className={styles.product__image}
+//           src={product.image}
+//           alt={product.title}
+//         />
   
-        <div className={styles.product__details}>
-          <p className={styles.details__title}>{product.title}</p>
-          <p className={styles.details__desc}>{product.description}</p>
-          <p className={styles.details__price}>$ {product.price}</p>
-        </div>
+//         <div className={styles.product__details}>
+//           <p className={styles.details__title}>{product.title}</p>
+//           <p className={styles.details__desc}>{product.description}</p>
+//           <p className={styles.details__price}>$ {product.price}</p>
+//         </div>
   
-        <div className={styles.product__buttons}>
-          <Link to={`/product/${product.id}`}>
-            <button
-              onClick={() => loadCurrentItem(product)}
-              className={`${styles.buttons__btn} ${styles.buttons__view}`}
-            >
-              View Item
-            </button>
-          </Link>
-          <button
-            onClick={() => addToCart(product.id)}
-            className={`${styles.buttons__btn} ${styles.buttons__add}`}
-          >
-            Add To Cart
-          </button>
-        </div>
-      </div>
-    );
-  };
+//         <div className={styles.product__buttons}>
+//           <Link to={`/product/${product.id}`}>
+//             <button
+//               onClick={() => loadCurrentItem(product)}
+//               className={`${styles.buttons__btn} ${styles.buttons__view}`}
+//             >
+//               View Item
+//             </button>
+//           </Link>
+//           <button
+//             onClick={() => addToCart(product.id)}
+//             className={`${styles.buttons__btn} ${styles.buttons__add}`}
+//           >
+//             Add To Cart
+//           </button>
+//         </div>
+//       </div>
+//     );
+//   };
 
-const mapDispatchToProps = (dispatch) => {
-    return {
-      addToCart: (id) => dispatch(addToCart(id)),
-      loadCurrentItem: (item) => dispatch(loadCurrentItem(item)),
-    };
-  };
+// const mapDispatchToProps = (dispatch) => {
+//     return {
+//       addToCart: (id) => dispatch(addToCart(id)),
+//       loadCurrentItem: (item) => dispatch(loadCurrentItem(item)),
+//     };
+//   };
   
-  export default connect(null, mapDispatchToProps)(Product);
+//   export default connect(null, mapDispatchToProps)(Product);
   
 
 
+
+const Product = () => {
+  return(
+
+<div className="column">
+<div className="productImage">
+<img className="ProductPicture" src={WineGlass} alt="img" />
+</div>
+<div className="productText">
+  <div className="Head">
+  <div className="chronicles_head">Plaza Chronicles</div> <div className="priceTag">$15</div><br></br>
+  </div>
+  <div>Wanna be cool? Get this.<br></br> Trust us.</div>
+</div>
+<div className="button">
+<Link to="/productDetails" className="Buy" ><div className="buyText">Buy</div></Link>
+</div>
+</div>
+  )
+};
+export default Product
