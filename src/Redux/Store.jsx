02f7d2d4b8710b36f7a2da258dc97e1d56e5dc51
palindrome-kasak <64642473+palindrome-kasak@users.noreply.github.com>
2022-03-19 23:33:18 +0530

    store

--- src/Redux/Store.jsx
+++ src/Redux/Store.jsx
@@ -1,7 +1,15 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import logger from 'redux-logger'
+import thunk from 'redux-thunk'
 // import { composeWithDevTools } from "redux-devtools-extension";
 import rootReducer from "./RootReducer";
 
-const store = createStore(rootReducer);
+
+const middleware = {thunk};
+const store = createStore(rootReducer, applyMiddleware(...middleware));
+
+if(process.env.NODE_ENV==="development"){
+    middleware.push(logger)
+}
 
 export default store;
