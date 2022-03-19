c0c0803823b5a4510f73b020251d8a6d3d5e09d1
palindrome-kasak <64642473+palindrome-kasak@users.noreply.github.com>
2022-03-19 23:33:00 +0530

    rootReducer

--- src/Redux/RootReducer.jsx
+++ src/Redux/RootReducer.jsx
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
+import userReducer from "./authReducer";
 
 import shoppingReducer from "./Shopping/ShoppingReducer";
 
 const rootReducer = combineReducers({
   shop: shoppingReducer,
+  user: userReducer,
 });
 
 export default rootReducer;
