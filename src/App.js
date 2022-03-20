4289bb5c8316020a55dff5a657aa383a3893c7a1
palindrome-kasak <64642473+palindrome-kasak@users.noreply.github.com>
2022-03-20 21:58:52 +0530

    app update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import React,{useState} from 'react'
 // import NavBar from '../src/Components/NavBar'
 import NavBar from "../src/Components/fixedComponents/NavigationBar";
 import { Route, Routes,Navigate } from "react-router-dom";
@@ -11,25 +12,54 @@ import { connect } from "react-redux";
 import Cart from "../src/pages/MyCartPage";
 
 import MyAccount from '../src/pages/MyProfilePages/MyAccount'
+import Login from "./pages/Login";
+
+import {firebase} from './firebase/firebase'
 
 function App({current}) {
+ 
+  const [IsUserSignedIn, setIsUserSignedIn ] = useState(true);
+firebase.auth().onAuthStateChanged((user)=>{
+  if(user){
+    return setIsUserSignedIn(true);
+  }
+  setIsUserSignedIn(false);
+})
+
+ if(IsUserSignedIn === true){
   return (
     <div>
       <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
-
+        {/* <Route path="/login" element={<Login />} /> */}
         <Route path="/home" element={<Home />} />
         <Route path="/shop" element={<Shop />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/profile" element={<MyAccount/>} />
         <Route path="/productDetails/:id" element={!current ? <Navigate to="/" /> : <ProductDetails/>} />
       </Routes>
+    </div>
+  );
+ }
+ else{
+  return (
+    <div>
+      {/* <NavBar /> */}
+      <Routes>
+        {/* <Route path="/" element={<Home />} /> */}
+        <Route path="/" element={<Login />} />
+        {/* <Route path="/home" element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/profile" element={<MyAccount/>} />
+        <Route path="/productDetails/:id" element={!current ? <Navigate to="/" /> : <ProductDetails/>} /> */}
+      </Routes>
      
       
     </div>
   );
-}
+}}
 const mapStateToProps = (state) => {
   return {
     current: state.shop.currentItem,
